67c63cebf1071f38ae3de14bbbb9122d194666a9
tim-ault <tault@innovative-technology.com>
2017-07-03 09:25:32 +0100

    20170703

---
+++ SPFLengthCorrector/MySql/CMySql.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SPFLengthCorrector
+{
+    class CMySql
+    {
+        private string m_ConnectionString;
+        MySqlConnection m_Connection;
+
+        private List<string> m_ByteLookup;
+
+        private const string SERVER = "10.10.200.20";
+        private const string USER_ID = "root";
+        private const string PASSWORD = "muppet";
+
+        public CMySql(string database)
+        {
+            m_ConnectionString = CreateConnectionString(SERVER, database, USER_ID, PASSWORD);
+            m_Connection = new MySqlConnection(m_ConnectionString);
+            m_ByteLookup = CreateByteLookup();
+
+        }
+
+        /// <summary>
+        /// Creates a lookup table for conversion of sensor data to a string for database storage.
+        /// </summary>
+        /// <returns>List of strings.</returns>
+        private List<string> CreateByteLookup()
+        {
+            List<string> ByteLookup = new List<string>();
+            for (int i = 0; i < 256; i++)
+            {
+                string ByteString = i.ToString("X2");
+                ByteLookup.Add(ByteString);
+
+            }
+            return ByteLookup;
+        }
+
+        private string CreateConnectionString(string server, string database, string userId, string password)
+        {
+            MySqlConnectionStringBuilder myCSB = new MySqlConnectionStringBuilder();
+            myCSB.Server = server;
+            myCSB.Database = database;
+            myCSB.UserID = userId;
+            myCSB.Password = password;
+
+            return myCSB.ConnectionString;
+        }
+
+        public List<int> GetCorruptDataIDs(string table, int faceType)
+        {
+            List<int> CorruptDataIDs = new List<int>();
+            MySqlCommand Command = m_Connection.CreateCommand();
+            MySqlDataReader myReader;
+
+            try
+            {
+                m_Connection.Open();
+                //Command.CommandText = string.Format(@"SELECT id FROM {0} WHERE Source = 4 AND Valid = 'False' AND Face_Type = {1} ", table, faceType);
+                Command.CommandText = string.Format(@"SELECT id FROM {0} WHERE Source = 4 AND Face_Type = {1} ", table, faceType);
+
+                myReader = Command.ExecuteReader();
+                if (myReader.HasRows)
+                {
+                    while (myReader.Read())
+                    {
+
+                        CorruptDataIDs.Add(myReader.GetInt32("id"));
+                    }
+                }
+                myReader.Close();
+                m_Connection.Close();
+            }
+
+            catch (Exception e)
+            {
+                m_Connection.Close();
+            }
+
+
+
+            return CorruptDataIDs;
+        }
+
+        public int GetSensorLength(string table, int id, int sensorDataLength,out int currentNoteLength)
+        {
+            byte[] SensorDataBlob = GetSensorDataBlob(table, id,out currentNoteLength);
+            int SensorLength = GetSensorDataLength(SensorDataBlob, sensorDataLength);
+            return SensorLength;
+        }
+
+
+        private byte[] GetSensorDataBlob(string table, int id, out int currentNoteLength)
+        {
+            currentNoteLength = 0;
+            MySqlCommand Command = m_Connection.CreateCommand();
+            MySqlDataReader myReader;
+            byte[] BlobData = new byte[0];
+
+            try
+            {
+
+                Command.CommandText = string.Format(@"SELECT LENGTH(Note_Data) FROM {0} WHERE ID = {1}", table, id);
+                m_Connection.Open();
+                int BlobLength = Convert.ToInt32(Command.ExecuteScalar());
+                BlobData = new byte[BlobLength];
+
+
+                Command.CommandText = string.Format(@"SELECT * FROM {0} WHERE ID = {1}", table, id);
+                myReader = Command.ExecuteReader();
+                if (myReader.HasRows)
+                {
+                    myReader.Read();
+                    myReader.GetBytes(myReader.GetOrdinal("Note_Data"), 0, BlobData, 0, BlobLength);
+                    currentNoteLength = myReader.GetInt32("Note_Length");
+                }
+                myReader.Close();
+                m_Connection.Close();
+            }
+
+            catch (Exception e)
+            {
+                m_Connection.Close();
+            }
+
+            return BlobData;
+        }
+
+        private int GetSensorDataLength(byte[] sensorDataBlob, int sensorDataLength)
+        {
+            int StartIndex = 0;
+            int EndIndex = 0;
+            int SensorDataLength = 0;
+            int BlobIndex = 0;
+
+
+            while (sensorDataBlob[BlobIndex] == 0x00)
+            {
+                BlobIndex++;
+            }
+
+            StartIndex = BlobIndex;
+
+            while (sensorDataBlob[BlobIndex] != 0x00)
+            {
+                BlobIndex++;
+            }
+
+            EndIndex = BlobIndex;
+
+            SensorDataLength = EndIndex - StartIndex;
+
+            return SensorDataLength;
+
+        }
+
+        public bool OverwriteSensorLength(string table, int id,int sensorLength)
+        {
+            MySqlCommand Command = m_Connection.CreateCommand();
+
+            try
+            {
+                Command.CommandText = string.Format(@"UPDATE {0} SET Note_Length = {1} WHERE id = {2}", table, sensorLength.ToString(), id.ToString());
+                m_Connection.Open();
+                Command.ExecuteNonQuery();
+                m_Connection.Close();
+
+            }
+            catch (Exception e)
+            {
+                m_Connection.Close();
+                return false;
+            }
+            m_Connection.Close();
+            return true;
+
+        }
+
+
+        public bool OverWriteSensorDataBlob(string table, int id, byte[] blobDataByteArray)
+        {
+
+            string BlobDataString = ConvertByteArrayToHexString(blobDataByteArray);
+            MySqlCommand Command = m_Connection.CreateCommand();
+
+            try
+            {
+                Command.CommandText = string.Format(@"UPDATE {0} SET Note_Data = {1}, CommentID = 1001 WHERE id = {2}", table, BlobDataString, id);
+                m_Connection.Open();
+                Command.ExecuteNonQuery();
+                m_Connection.Close();
+
+            }
+            catch (Exception e)
+            {
+                m_Connection.Close();
+                return false;
+            }
+            m_Connection.Close();
+            return true;
+
+        }
+
+        /// <summary>
+        /// Converts a byte array to a string of non delimited hex.
+        /// </summary>
+        /// <param name="byteArray">Byte array input</param>
+        /// <returns>A string of non delimited hex.</returns>
+        private string ConvertByteArrayToHexString(byte[] byteArray)
+        {
+            StringBuilder SbHex = new StringBuilder((byteArray.Length + 1) * 2);
+            SbHex.Append("0x");
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                SbHex.Append(m_ByteLookup[byteArray[i]]);
+            }
+
+
+            return SbHex.ToString();
+        }
+
+
+
+
+
+    }
+}
---
+++ SPFLengthCorrector/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPFLengthCorrector
+{
+    public enum NoteFace
+    {
+        FUFF = 1,    // Face up, face first
+        FUFL,       // Face up, face last
+        FDFF,       // Face down, face first
+        FDFL        // Face down, face last
+    }
+
+
+
+    class Program
+    {
+        static CMySql m_MySql;
+        public const byte SENSOR_DATA_LENGTH = 50;
+
+        static void Main(string[] args)
+        {
+            string Database = args[0];
+            string Table = args[1];
+            int Face = int.Parse(args[2]);
+            
+
+            string FaceString = ((NoteFace)Face).ToString();
+
+            int CalculatedSensorDataLength = 0;
+            int CurrentSensorDataLength = 0;
+            int MaxSensorDataLength = 0;
+            int MinSensorDataLength = SENSOR_DATA_LENGTH;
+            int NumberOfMismatches = 0;
+
+            m_MySql = new CMySql(Database);
+            Console.WriteLine("Finding Records");
+            List<int> IDs = m_MySql.GetCorruptDataIDs(Table, Face);
+            Console.WriteLine(IDs.Count.ToString() + " Found");
+
+            foreach(int ID in IDs)
+            {
+                CalculatedSensorDataLength = m_MySql.GetSensorLength(Table, ID, SENSOR_DATA_LENGTH, out CurrentSensorDataLength);
+                Console.Write("ID " + ID.ToString() + " Length: " + CalculatedSensorDataLength.ToString() + "\\" + CurrentSensorDataLength.ToString());
+
+                //if (CalculatedSensorDataLength == 3)
+                //{
+                //    Console.WriteLine("222");
+                //}
+
+                if (CalculatedSensorDataLength != CurrentSensorDataLength)
+                {
+                    Console.WriteLine("***");
+                    NumberOfMismatches++;
+                    m_MySql.OverwriteSensorLength(Table, ID, CalculatedSensorDataLength);
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
+
+                MaxSensorDataLength = Math.Max(MaxSensorDataLength, CalculatedSensorDataLength);
+                MinSensorDataLength = Math.Min(MinSensorDataLength, CalculatedSensorDataLength);
+            }
+            Console.WriteLine("");
+            Console.WriteLine(Database.ToString() + "." + Table.ToString() + " " + Face.ToString() +" ("+  FaceString+ ")");
+            Console.WriteLine("Records Found: " + IDs.Count.ToString() );
+            Console.WriteLine("Mismatches: " + NumberOfMismatches.ToString());
+            Console.WriteLine("Max Sensor Length: " + MaxSensorDataLength.ToString());
+            Console.WriteLine("Min Sensor Length: " + MinSensorDataLength.ToString());
+        }
+    }
+}
